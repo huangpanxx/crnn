caf993eeeda48cc6662c9a19091a8fd0828e29e9
huangpanxx <ls.snail@gmail.com>
2015-04-13 08:43:28 +0800

    .

--- CRNN.gui/Program.cs
+++ CRNN.gui/Program.cs
@@ -27,45 +27,26 @@ namespace CRNN.gui
         {
             if (args.Length != 2 || !args[1].EndsWith(".json"))
                 return false;
-            var filename = args[1];
-            var json = File.ReadAllText(filename);
-            using (Network network = new Network(json, "predict"))
-            {
-                while (true)
-                {
-                    Console.Write("IMAGE:");
-                    var path = Console.ReadLine();
-                    if (!File.Exists(path)) continue;
-                    using (var image = Image.FromFile(path))
-                    {
-                        var data = Utility.ImageToFloatArray(image);
-                        network.SetInput(data);
-                        for (int i = 0; i < 20; ++i)
-                        {
-                            using (var signal = network.Forward())
-                            {
-                                int k = signal.ArgMax();
-                                var s = network.Translate(k);
-                                if (s == "eof") break;
-                                Console.Write(s);
-                            }
-                        }
-                        Console.WriteLine();
-                    }
-                }
-            }
+            Network.TrainAndTestNetwork(args[1]);
+            return true;
         }
 
         static bool RunServer(string[] args)
         {
             if (args.Length != 1) return false;
-            var filename = Utility.PromoteLine("MODEL");
-            var json = File.ReadAllText(filename);
-            using (CaptchaEngine engine = new CaptchaEngine())
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "MODEL|*.json";
+            dlg.Title = "MODEL";
+            if (dlg.ShowDialog() == DialogResult.OK)
             {
-                engine.LoadModel(json, "predict");
-                CaptchaServer server = new CaptchaServer(engine);
-                server.Start(7500);
+                var filename = dlg.FileName;
+                var json = File.ReadAllText(filename);
+                using (CaptchaEngine engine = new CaptchaEngine())
+                {
+                    engine.LoadModel(json, "predict");
+                    CaptchaServer server = new CaptchaServer(engine);
+                    server.Start(7500);
+                }
             }
             return true;
         }
